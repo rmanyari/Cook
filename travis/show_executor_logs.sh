ed40684c0ecde6b5ca10f736f9490fea28a0bcf8
Paul Schorfheide <paul.schorfheide@twosigma.com>
2017-09-12 16:10:55 -0500

    Feedback from @dposada

--- travis/show_executor_logs.sh
+++ travis/show_executor_logs.sh
@@ -8,6 +8,8 @@ while read path; do
     echo "------------------------------------"
 done <<< "$(find $PROJECT_DIR/../travis/.minimesos -name 'stdout' -o -name 'stderr' -o -name 'executor.log')"
 
-echo "Contents of scheduler/log/cook.log"
-cat $PROJECT_DIR/../scheduler/log/cook.log
-echo "------------------------------------"
+while read path; do
+    echo "Contents of ${path}"
+    cat "${path}";
+    echo "------------------------------------"
+done <<< "$(find $PROJECT_DIR/../scheduler/log -name 'cook*.log')"
