293863276c8fcfd3cf63401d1288ddf86e4c65a0
Paul Schorfheide <paul.schorfheide@twosigma.com>
2017-09-13 08:51:01 -0500

    wip trying to fix travis

--- travis/show_executor_logs.sh
+++ travis/show_executor_logs.sh
@@ -8,6 +8,9 @@ while read path; do
     echo "------------------------------------"
 done <<< "$(find $PROJECT_DIR/../travis/.minimesos -name 'stdout' -o -name 'stderr' -o -name 'executor.log')"
 
+echo "what logs are there"
+ls $PROJECT_DIR/../scheduler/log -name 'cook*.log'
+
 while read path; do
     echo "Contents of ${path}"
     cat "${path}";
